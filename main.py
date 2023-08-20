f93e9cd92f249c6a949a89d6c964cde7d054f9c4
yahiashamaa <yehiashamaa987@gmail.com>
2023-08-20 22:38:28 +0300

    Subtraction function

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #TODO Calculator FUNCTIONS
 
-
-
-
-
-
 def calculate(a, b, operation):
         #TODO (CONTINUE ON THE SAME PATTERN
+        if operation == "-":
+                return a - b
+
+                
 
 
 
 
-num1 = #TODO INPUT FROM USER
-num2 = #TODO INPUT FROM USER
-op = #TODO INPUT FROM USER
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+op = input("Operator: ")
 print(calculate(num1, num2, op))
